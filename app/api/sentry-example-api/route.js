56d2f3d3f54c6a43a7db20d729b6e0ae8228f900
Kinyera Alvine <111269133+alvine2@users.noreply.github.com>
2024-08-09 15:24:36 +0300

    Update route.js

--- app/api/sentry-example-api/route.js
+++ app/api/sentry-example-api/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-// A faulty API route to test Sentry's error monitoring
+// A faulty API route to test Sentry's error monitoring add.alvine.tech
 export function GET() {
   throw new Error("Sentry Example API Route Error");
   return NextResponse.json({ data: "Testing Sentry Error..." });
